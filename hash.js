433f1bedb3920eaddc6ed1573c468f22ad190721
peerichie <peerichie9@gmail.com>
2020-02-07 03:51:19 +0100

    Built a fizzbuzz challenge with Javascript

---
+++ hash.js
@@ -0,0 +1,11 @@
+//hash loop
+let number = "#";
+while (number <= 7) {
+console.log(number);
+number = number + "#";
+}
+let hash = "#";
+for (let hashNum = 0; hashNum < 7; hashNum++ ) {
+    console.log(hash);
+    hash += "#"
+}
